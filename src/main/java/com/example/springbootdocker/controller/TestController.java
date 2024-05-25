7082cfe5e12429718601b23176e1cad4f3423c1b
CloseSu <wei33107@gmail.com>
2024-05-25 23:26:39 +0800

    add

--- src/main/java/com/example/springbootdocker/controller/TestController.java
+++ src/main/java/com/example/springbootdocker/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.example.springbootdocker.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @GetMapping("/getTestMsg")
     public String getTestMsg() {
-        return "test spring boot success";
+        String msg = "test spring boot success";
+        for (int i = 0; i < 30; i++) {
+            log.info(msg);
+        }
+        return msg;
     }
     @GetMapping("/slow")
     public String slow() throws InterruptedException {
